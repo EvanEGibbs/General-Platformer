4dc72b42a02a7d717744ceb813cddb614cffe6b7
Evan Gibbs <evanegibbs@gmail.com>
2017-06-07 23:34:12 -0700

    Wall Jump (video 9)

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -11,19 +11,24 @@ public class Controller2D : RaycastController {
 
 	public override void Start() {
 		base.Start();
+		collisions.faceDirection = 1;
 	}
 
 	public void Move(Vector3 velocity, bool standingOnPlatform = false) {
 		UpdateRaycastOrigins();
 		collisions.Reset();
 		collisions.velocityOld = velocity;
+
+		if (velocity.x != 0) {
+			collisions.faceDirection = (int)Mathf.Sign(velocity.x);
+		}
 		
 		if (velocity.y < 0) {
 			DescendSlope(ref velocity);
 		}
-		if (velocity.x != 0) {
-			HorizontalCollisions(ref velocity);
-		}
+
+		HorizontalCollisions(ref velocity);
+
 		if (velocity.y != 0) {
 			VerticalCollisions(ref velocity);
 		}
@@ -36,8 +41,13 @@ public class Controller2D : RaycastController {
 	}
 
 	void HorizontalCollisions(ref Vector3 velocity) {
-		float directionX = Mathf.Sign(velocity.x);
+		float directionX = collisions.faceDirection;
 		float rayLength = Mathf.Abs(velocity.x) + SKIN_WIDTH;
+
+		if (Mathf.Abs(velocity.x) < SKIN_WIDTH){
+			rayLength = 2 * SKIN_WIDTH;
+		}
+
 		for (int i = 0; i < horizontalRayCount; i++) {
 			Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
 			rayOrigin += Vector2.up * (horizontalRaySpacing * i);
@@ -167,6 +177,7 @@ public class Controller2D : RaycastController {
 		public bool descendingSlope;
 		public float slopeAngle, slopeAngleOld;
 		public Vector3 velocityOld;
+		public int faceDirection;
 
 		public void Reset() {
 			above = below = false;
